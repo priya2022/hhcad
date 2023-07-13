eb630a255affee64ad29dc4825c67a90da50ca14
priya_n2022 <glenraise@gmail.com>
2023-07-13 16:27:20 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {styled} from '@mui/system'
+// import {Button}
+
+function App() {
+
+  return (
+    <>
+      <div>
+         <h2>Okkay</h2>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
